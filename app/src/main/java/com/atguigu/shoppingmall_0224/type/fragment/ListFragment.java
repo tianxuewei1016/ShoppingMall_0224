13a5f4e03a36c55c66fda0866cf240f94da51f38
tianxuewei1016 <93226539@qq.com>
2017-06-16 16:34:13 +0800

    设置ListView的适配器TypeLeftAdapter

--- app/src/main/java/com/atguigu/shoppingmall_0224/type/fragment/ListFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall_0224/type/fragment/ListFragment.java
@@ -1,12 +1,15 @@
 package com.atguigu.shoppingmall_0224.type.fragment;
 
-import android.graphics.Color;
-import android.util.Log;
-import android.view.Gravity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import com.atguigu.shoppingmall_0224.R;
 import com.atguigu.shoppingmall_0224.base.BaseFragment;
+import com.atguigu.shoppingmall_0224.type.adapter.TypeLeftAdapter;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * 作者：田学伟 on 2017/6/16 15:40
@@ -14,16 +17,23 @@ import com.atguigu.shoppingmall_0224.base.BaseFragment;
  * 作用：分类
  */
 
-public class ListFragment extends BaseFragment{
-    private TextView textView;
+public class ListFragment extends BaseFragment {
 
+
+    @InjectView(R.id.lv_left)
+    ListView lvLeft;
+    @InjectView(R.id.rv_right)
+    RecyclerView rvRight;
+    //网络请求得到数据
+    private String[] titles = new String[]{"小裙子", "上衣", "下装", "外套", "配件", "包包", "装扮", "居家宅品",
+            "办公文具", "数码周边", "游戏专区"};
+
+    private TypeLeftAdapter adapter;
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_list, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     /**
@@ -33,7 +43,14 @@ public class ListFragment extends BaseFragment{
     @Override
     public void initData() {
         super.initData();
-        Log.e("TAG", "分类的数据被初始化了...");
-        textView.setText("分类内容");
+        //设置左边的适配器
+        adapter = new TypeLeftAdapter(mContext,titles);
+        lvLeft.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
