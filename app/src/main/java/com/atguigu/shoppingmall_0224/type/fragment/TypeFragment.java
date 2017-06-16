139a0ffc73671ab45292cc74de439330047a9890
tianxuewei1016 <93226539@qq.com>
2017-06-16 16:00:01 +0800

    FlycoTabLayoutde ,创建ListFragment和TagFragemnt并且切换

--- app/src/main/java/com/atguigu/shoppingmall_0224/type/fragment/TypeFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall_0224/type/fragment/TypeFragment.java
@@ -1,12 +1,13 @@
 package com.atguigu.shoppingmall_0224.type.fragment;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.atguigu.shoppingmall_0224.R;
+import com.atguigu.shoppingmall_0224.activity.MainActivity;
 import com.atguigu.shoppingmall_0224.base.BaseFragment;
 import com.flyco.tablayout.SegmentTabLayout;
 import com.flyco.tablayout.listener.OnTabSelectListener;
@@ -53,17 +54,60 @@ public class TypeFragment extends BaseFragment {
     @Override
     public void initData() {
         super.initData();
+
         //设置内容
         tl1.setTabData(titles);
         //监听Tab的状态
         tl1.setOnTabSelectListener(new MyOnTabSelectListener());
+        initFragment();
+        switchFragment(fragments.get(0));
+    }
+
+    private void switchFragment(Fragment currentFragment) {
+        //切换的不是同一个页面
+        if (tempFragment != currentFragment) {
+
+            MainActivity activity = (MainActivity) mContext;
+            //得到FragmentMager
+            FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
+            //如果没有添加就添加
+            if (!currentFragment.isAdded()) {
+                //缓存的隐藏
+                if (tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                //添加
+                ft.add(R.id.fl_type, currentFragment);
+            } else {
+                //缓存的隐藏
+                if (tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                //显示
+                ft.show(currentFragment);
+            }
+            //事务提交
+            ft.commit();
+            //把当前的赋值成缓存的
+            tempFragment = currentFragment;
+        }
+    }
+
+    /**
+     * 初始化Fragment
+     */
+    private void initFragment() {
+        fragments = new ArrayList<>();
+        fragments.add(new ListFragment());
+        fragments.add(new TagFragment());
     }
 
     class MyOnTabSelectListener implements OnTabSelectListener {
 
         @Override
         public void onTabSelect(int position) {
-            Toast.makeText(mContext, "position==" + position, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(mContext, "position==" + position, Toast.LENGTH_SHORT).show();
+            switchFragment(fragments.get(position));
         }
 
         @Override
@@ -77,7 +121,6 @@ public class TypeFragment extends BaseFragment {
         super.onDestroyView();
         ButterKnife.reset(this);
     }
-
 
     @OnClick(R.id.iv_type_search)
     public void onViewClicked() {
